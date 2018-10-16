2eb1ac23b201be1e13ef45d082ed7a0c18216cd8
mac <f.lebon@sciencesu-lyon.fr>
2018-10-16 11:18:16 +0200

    2e commit

--- ComparateurJeux/src/AppBundle/Controller/DefaultController.php
+++ ComparateurJeux/src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $uri = 'http://api.football-data.org/v2/competitions/PL/matches/?matchday=22';
+        $reqPrefs['http']['method'] = 'GET';
+        $reqPrefs['http']['header'] = 'X-Auth-Token: d80c0fd9af4e43a2ae503d156f34d34e';
+        $stream_context = stream_context_create($reqPrefs);
+        $response = file_get_contents($uri, false, $stream_context);
+        $matches = json_decode($response);
+        var_dump($matches);
+        die();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
