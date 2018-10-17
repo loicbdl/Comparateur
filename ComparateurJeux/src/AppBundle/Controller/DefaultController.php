b893e4d77d5713b1f0072ced6fa86dbc5b090cf1
mac <f.lebon@sciencesu-lyon.fr>
2018-10-17 10:27:21 +0200

    3e commit

--- ComparateurJeux/src/AppBundle/Controller/DefaultController.php
+++ ComparateurJeux/src/AppBundle/Controller/DefaultController.php
@@ -13,14 +13,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $uri = 'http://api.football-data.org/v2/competitions/PL/matches/?matchday=22';
+        $uri = 'http://api.football-data.org/v2/competitions';
         $reqPrefs['http']['method'] = 'GET';
         $reqPrefs['http']['header'] = 'X-Auth-Token: d80c0fd9af4e43a2ae503d156f34d34e';
         $stream_context = stream_context_create($reqPrefs);
         $response = file_get_contents($uri, false, $stream_context);
         $matches = json_decode($response);
-        var_dump($matches);
-        die();
+        // var_dump($matches->competitions);
+
+        foreach ($matches->competitions as $championnat){
+            echo $championnat->name . '<br>';
+        }
+         die();
+
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
